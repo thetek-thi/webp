cd4835106e615208955461d966533d83f9c4fd8b
thetek-thi <git-thi@thetek.de>
2022-11-08 10:40:17 +0100

    js file

---
+++ p3/scripts/main.js
@@ -0,0 +1,17 @@
+/*
+ * TODO auslagern (??)
+ *
+ * COLLECTION ID
+ *  - 288e633a-bebc-4bcc-80b8-4d6f4226b644
+ *
+ * USER TOM
+ *  - uname: Tom
+ *  - pw: c3f39e8d
+ *  - token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjY3ODk5NDExfQ.XIPoLdoBdBDzM13ivyXFrCEgXlg1CBOcxrYVPIDrUO8
+ *
+ * USER JERRY
+ *  - uname: Jerry
+ *  - pw: 74bd67be
+ *  - token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2Njc4OTk0MTF9.fJVvhRn2Wpme--QuNtBk623ff3ES-nZOUwI6PBu9Qyg
+ */
+

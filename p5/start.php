866b05f1219dde76110faa56d7d81f95ff72240b
thetek42 <git@thetek.de>
2022-12-19 10:39:08 +0100

    p5 c: finish c

--- p5/start.php
+++ p5/start.php
@@ -7,4 +7,7 @@ session_start();
 
 define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat');
 define('CHAT_SERVER_ID', '56ce2af0-ee84-4e78-85bc-6bba6c51c739');
+
+require 'Utils/BackendService.php';
+$service = new Utils\BackendService(CHAT_SERVER_URL, CHAT_SERVER_ID);
 ?>
